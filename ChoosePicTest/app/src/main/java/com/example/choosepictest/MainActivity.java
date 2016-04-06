7f60d32db1e20fc4f863ad14eaec1b72f79ee2d1
Nick Ye <yefenglin2011@gmail.com>
2016-04-06 14:55:07 +0800

    修改工程：ChoosePicTest

--- ChoosePicTest/app/src/main/java/com/example/choosepictest/MainActivity.java
+++ ChoosePicTest/app/src/main/java/com/example/choosepictest/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.choosepictest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import static android.util.Log.d;
 
 
 public class MainActivity extends Activity {
-
+    private static final String TAG = "Nick-MainActivity";
     private static final int TAKE_PICTURE = 1;
     private static final int IMAGE_CROP = 2;
     private Button takePic;
@@ -30,6 +36,7 @@ public class MainActivity extends Activity {
         takePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                d(TAG, "MainActivity.onClick."+Environment.getExternalStorageDirectory().getPath());
                 File outputImage = new File(Environment.getExternalStorageDirectory(),"tempImage.jpg");
                 try {
                     if (outputImage.exists()){
@@ -54,7 +61,7 @@ public class MainActivity extends Activity {
         switch (requestCode){
             case TAKE_PICTURE:
                 if (resultCode==RESULT_OK) {
-                    Intent intent = new Intent("com.android.camera.action.IMAGE_CROP");
+                    Intent intent = new Intent("com.android.camera.action.CROP");
                     intent.setDataAndType(imageUri, "image/*");
                     intent.putExtra("scale", true);
                     intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
@@ -62,9 +69,17 @@ public class MainActivity extends Activity {
                 }
                 break;
             case IMAGE_CROP:
+                Log.d(TAG, "MainActivity.onActivityResult.IMAGE_CROP:resultCode ="+ resultCode);
                 if (resultCode==RESULT_OK){
-
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
+                break;
+            default:
                 break;
 
         }
