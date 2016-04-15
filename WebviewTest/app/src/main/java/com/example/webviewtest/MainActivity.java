aec9bf91b08e0f92999a75b513135f8d524fe500
yfl007 <yefenglin2011@gmail.com>
2016-04-15 15:24:19 +0800

    添加工程：WebviewTest

---
+++ WebviewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.webviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl("http://www.baidu.com");
+
+    }
+
+}
